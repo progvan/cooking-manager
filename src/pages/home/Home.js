f23ee2b752aa90087c4b6bff807b64812df3c87e
VanUA <vanatom22@gmail.com>
2024-12-05 20:00:12 +0200

    Implement Home component with data fetching and styling

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,15 @@
 import "./Home.css";
 import React from "react";
+import { useFetch } from "../../hooks/useFetch";
 
 export default function Home() {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && data.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+    </div>
+  );
 }
